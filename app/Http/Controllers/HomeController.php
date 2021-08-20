851f9bc81f7f734eb1be6ddf09b52e9a4fe5779e
teitsuki <teitsuki@icloud.com>
2021-08-20 10:28:48 +0900

    タグ作成機能の開発

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Memo;
+use App\Models\Tag;
+use App\Models\MemoTag;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -25,41 +29,82 @@ class HomeController extends Controller
     public function index()
     {
         $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
+            ->where('user_id', '=', Auth::id())
             ->whereNull('deleted_at')
             ->orderBY('updated_at', 'DESC')
             ->get();
 
-        return view('create', compact('memos'));
+        $tags = Tag::where('user_id', '=', Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+        return view('create', compact('memos', 'tags'));
     }
-    
+
     public function store(Request $request)
     {
         $posts = $request->all();
+
+        DB::transaction(function () use ($posts) {
+            $memo_id = Memo::insertGetId(['content' => $posts['content'], 'user_id' => Auth::id()]);
+            $tag_exists = Tag::where('user_id', '=', Auth::id())->where('name', '=', $posts['new_tag'])->exists();
+            if (!empty($posts['new_tag']) && !$tag_exists) {
+                $tag_id = Tag::insertGetId(['user_id' => Auth::id(), 'name' => $posts['new_tag']]);
+                MemoTag::insert(['memo_id' => $memo_id, 'tag_id' => $tag_id]);
+            }
+            if (!empty($posts['tags'][0])) {
+                foreach ($posts['tags'] as $tag) {
+                    MemoTag::insert(['memo_id' => $memo_id, 'tag_id' => $tag]);
+                }
+            }
+        });
         
-        Memo::insert(['content' => $posts['content'], 'user_id' => \Auth::id()]);
         return redirect(route('home'));
     }
     
     public function edit($id)
     {
         $memos = Memo::select('memos.*')
-        ->where('user_id', '=', \Auth::id())
+        ->where('user_id', '=', Auth::id())
         ->whereNull('deleted_at')
         ->orderBY('updated_at', 'DESC')
         ->get();
+
+        $edit_memo = Memo::select('memos.*', 'tags.id AS tag_id')
+        ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+        ->leftJoin('tags', 'memo_tags.tag_id', '=', 'tags.id')
+        ->where('memos.user_id', '=', Auth::id())
+            ->where('memos.id', '=', $id)
+            ->whereNull('memos.deleted_at')
+            ->get();
+
+        $include_tags = [];
+        foreach ($edit_memo as $memo) {
+            array_push($include_tags, $memo['tag_id']);
+        }
+
+        $tags = Tag::where('user_id', '=', Auth::id())->whereNull('deleted_at')->orderBy('id', 'DESC')->get();
         
-        $edit_memo = Memo::find($id);
-        
-        return view('edit', compact('memos', 'edit_memo'));
+        return view('edit', compact('memos', 'edit_memo', 'include_tags', 'tags'));
     }
 
     public function update(Request $request)
     {
         $posts = $request->all();
-        
-        Memo::where('id', $posts['memo_id'])
-            ->update(['content' => $posts['content']]);
+
+        DB::transaction(function () use ($posts) {
+            Memo::where('id', $posts['memo_id'])->update(['content' => $posts['content']]);
+            MemoTag::where('memo_id', '=', $posts['memo_id'])->delete();
+            foreach ($posts['tags'] as $tag) {
+                MemoTag::insert(['memo_id' => $posts['memo_id'], 'tag_id' => $tag]);
+            }
+            $tag_exists = Tag::where('user_id', '=', Auth::id())->where('name', '=', $posts['new_tag'])->exists();
+            if (!empty($posts['new_tag']) && !$tag_exists) {
+                $tag_id = Tag::insertGetId(['user_id' => Auth::id(), 'name' => $posts['new_tag']]);
+                MemoTag::insert(['memo_id' => $posts['memo_id'], 'tag_id' => $tag_id]);
+            }
+        });
 
         return redirect(route('home'));
     }
